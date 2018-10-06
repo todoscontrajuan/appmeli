6567ffdb10487ba9ae4e677edf5f32a55747e3f8
Juan Manuel Rodriguez <juan.m.rodriguez22@gmail.com>
2018-10-06 19:20:01 -0300

    Fonts and landing page with searchview

---
+++ app/src/main/java/com/me/squad/appmeli/LandingPageFragment.java
@@ -0,0 +1,19 @@
+package com.me.squad.appmeli;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Fragment that contains the landing page of the app
+ */
+public class LandingPageFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_landing_page, container, false);
+    }
+}
--- app/src/main/java/com/me/squad/appmeli/MainActivity.java
+++ app/src/main/java/com/me/squad/appmeli/MainActivity.java
@@ -1,8 +1,15 @@
 package com.me.squad.appmeli;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Show landing page by default
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, new LandingPageFragment());
+        fragmentTransaction.commit();
+
+        // Search bar setup
+        final SearchView searchView = findViewById(R.id.search_bar);
+        searchView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchView.setIconified(false);
+            }
+        });
     }
 }
