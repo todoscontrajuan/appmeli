3e314f29d51b865d6839778a7604ffcf6f70cc0a
Juan Manuel Rodriguez <juan.m.rodriguez22@gmail.com>
2018-10-07 19:02:30 -0300

    Suggestions implemented - Toolbar implemented - Search result item implemented

--- app/src/main/java/com/me/squad/appmeli/MainActivity.java
+++ app/src/main/java/com/me/squad/appmeli/MainActivity.java
@@ -1,19 +1,27 @@
 package com.me.squad.appmeli;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    public final static String SEARCH_VALUE = "searchValue";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +38,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Find layout elements
         final FrameLayout fragmentContainer = findViewById(R.id.fragment_container);
-        final RecyclerView suggestionList = findViewById(R.id.suggestion_list);
+        final ListView suggestionList = findViewById(R.id.suggestion_list);
         final SearchView searchView = findViewById(R.id.search_bar);
+        ImageView closeButton = searchView.findViewById(R.id.search_close_btn);
 
         // Search bar listeners setup
         searchView.setOnClickListener(new View.OnClickListener() {
@@ -42,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
                 suggestionList.setVisibility(View.VISIBLE);
             }
         });
-        ImageView closeButton = searchView.findViewById(R.id.search_close_btn);
         closeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -51,16 +59,45 @@ public class MainActivity extends AppCompatActivity {
                 suggestionList.setVisibility(View.GONE);
             }
         });
+
+        // Setup search bar suggestions
+        ArrayList<String> suggestionElementsList = new ArrayList<>();
+        // TODO Set this strings in xml
+        suggestionElementsList.add("Autos y Motos");
+        suggestionElementsList.add("Computación");
+        suggestionElementsList.add("Inmuebles");
+        suggestionElementsList.add("Electrodomesticos");
+        suggestionElementsList.add("Juegos y Juguetes");
+        suggestionElementsList.add("Celulares");
+        suggestionElementsList.add("Ropa");
+        suggestionElementsList.add("Consolas y Videojuegos");
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.suggestion_list_item, suggestionElementsList);
+        suggestionList.setAdapter(adapter);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
-                return false;
+                performSearch(s);
+                return true;
             }
 
             @Override
             public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s);
                 return false;
             }
         });
+        suggestionList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String selected = ((TextView) view.findViewById(R.id.suggestion_text)).getText().toString();
+                performSearch(selected);
+            }
+        });
+    }
+
+    private void performSearch(String query) {
+        Intent intent = new Intent(getApplicationContext(), SearchResultActivity.class);
+        intent.putExtra(SEARCH_VALUE, query);
+        startActivity(intent);
     }
 }
